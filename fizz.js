751e299c7ff8327edb6459d85d7fe525cd467543
jesuskata <rzarate.alex@gmail.com>
2017-03-27 15:52:44 -0600

    Esta es la segunda propuesta para colocar la marca a los divisibles por 3 y 5 y para los divisibles independientes de 3 y 5

--- fizz.js
+++ fizz.js
@@ -1,22 +1,17 @@
 var numeros = 100;
-var divisible = false;
 
 for(var i = 1; i <= 100; i++)
 {
-    divisible = false;
     if(i % 3 == 0)
     {
         document.write("Fizz");
-        divisible = true;
     }
     
     if(i % 5 == 0)
     {
         document.write("Buzz");
-        divisible = true;
-    }
-    /*El signo de admiración significa negar lo que tiene por delante, en este caso está diciendo si 'no es divisible'*/
-    if (!divisible)
+    /*}En esta sección se está usando una operación matemática para los resultados divisibles de 3 y 5, y el compuesto && significa 'y'*/
+    if(i%3 != 0 && i%5 != 0)
     {
         document.write(i);
     }
